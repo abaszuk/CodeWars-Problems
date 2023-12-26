6d17ac7149fdc1755a3005ef69ccfa3c97c69fa0
Alex Baszuk <abaszuk@gmail.com>
2023-12-26 12:53:23 -0500

    tech elevator practice

--- src/String2Number.java
+++ src/String2Number.java
@@ -1,2 +1,32 @@
+import static java.lang.Integer.parseInt;
+
 public class String2Number {
+    public static int stringToNumber(String str) {
+        //TODO: Convert str into a number
+        int nums = parseInt(str);
+        return nums;
+    }
+
+    public static String twoStrings(String a, String b) {
+        if (b.length() > a.length()) {
+            return a + b.substring((b.length() - a.length()));
+        }
+        if (b.length() < a.length()) {
+            return a.substring((a.length() - b.length())) + b;
+        }
+        return a + b;
+    }
+
+    public static String xInTwo(String a){
+        if(a.charAt(0) == 'x' && a.charAt(1) == 'x'){
+            return a.substring(2);
+        }
+        if (a.charAt(0) == 'x'){
+            return a.substring(1);
+        }
+        if(a.charAt(1) == 'x'){
+            return a.substring(0,1) + a.substring(2);
+        }
+        return a;
+    }
 }
